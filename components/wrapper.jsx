bd5052ccd4b31a9dac9a230849386fe2db452c19
Yves-Developer <yvesmugisha09@gmail.com>
2025-03-13 21:23:02 +0200

    # screen optimised

--- components/wrapper.jsx
+++ components/wrapper.jsx
@@ -3,7 +3,12 @@ import React from "react";
 
 const Wrapper = ({ children, classNames }) => {
   return (
-    <div className={clsx("container mx-auto max-sm:max-w-[450px]", classNames)}>
+    <div
+      className={clsx(
+        "container lg:max-w-[1024] mx-auto max-sm:max-w-[450px]",
+        classNames
+      )}
+    >
       {children}
     </div>
   );
