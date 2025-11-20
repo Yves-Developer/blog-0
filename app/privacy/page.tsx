efc395fdbbc23726fbe82f38cddfdcbd1d17819d
Yves DC <97420310+Yves-Developer@users.noreply.github.com>
2025-11-20 20:05:18 +0200

    Update file path for privacy policy markdown

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -3,7 +3,7 @@ import path from "path";
 import Markdown from "react-markdown";
 
 export default function PrivacyPolicy() {
-  const filePath = path.join(process.cwd(), "PRIVACY_POLICY.md");
+  const filePath = path.join(process.cwd(), "public","PRIVACY_POLICY.md");
   const content = fs.readFileSync(filePath, "utf8");
 
   return (
